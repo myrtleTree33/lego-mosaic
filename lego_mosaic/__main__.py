0bc9a8d54a603a7f1233de612ec61e917920ce8b
myrtletree33 <me@joeltong.org>
2016-08-28 00:24:57 +0800

    added output filename

--- lego_mosaic/__main__.py
+++ lego_mosaic/__main__.py
@@ -4,9 +4,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Convert your pictures into a a Lego mosaic.')
-    parser.add_argument('input_filename', metavar='input_filename', type=str, nargs='+', help='The input image to convert')
-    parser.add_argument('output_filename', metavar='output_filename', type=str, nargs='+', help='The output image to save to')
-    parser.add_argument('--show', nargs='?', const=True, type=bool, default=False)
+    parser.add_argument('input_filename', metavar='input_filename', type=str, nargs='?', help='The input image to convert')
+    parser.add_argument('output_filename', metavar='output_filename', type=str, const='out.jpg', default='out.jpg', nargs='?', help='The output image to save to')
+    parser.add_argument('--show', nargs='?', const=True, type=bool, default=False, help='If toggled, displays output to screen')
     parser.add_argument('--size', nargs='?', const=5, type=int, default=5, help='The rendering length of each tile.')
     parser.add_argument('--length', nargs='?', const=48, type=int, default=48, help='The length of each mosaic.')
     parser.add_argument('--num_clusters', nargs='?', const=7, type=int, default=7, help='Number of color clusters to quantize')
@@ -17,8 +17,9 @@ def main():
     args = parser.parse_args()
     print args
 
-    input_filename = args.input_filename[0]
-    output_filename = args.output_filename[0]
+    input_filename = args.input_filename
+    output_filename = args.output_filename
+    print output_filename
     tile_size = args.size
     n = args.num_clusters
     img_length = args.length
@@ -29,6 +30,8 @@ def main():
     .apply_filter(QuantizeFilter(n)) \
     .apply_filter(ConstrainPaletteFilter('Lego-colors-palette-2010.gpl.csv')) \
     .apply_filter(BuildMapFilter(tile_size)) \
+    .save_file(output_filename)
+
 
     if args.show:
         img.show()
