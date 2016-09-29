b2872da339ffb1820605611927d1531e36b13172
myrtletree33 <me@joeltong.org>
2016-09-30 00:03:50 +0800

    added ability to generate color parts list json from pic

--- lego_mosaic/__main__.py
+++ lego_mosaic/__main__.py
@@ -12,6 +12,7 @@ import glob
 IP = 'tcp://0.0.0.0'
 PORT = '4000'
 
+
 def run_server(ip, port):
     # Run a server if specified
     address = ip + ":" + port
@@ -22,26 +23,34 @@ def run_server(ip, port):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Convert your pictures into a a Lego mosaic.')
+    parser = argparse.ArgumentParser(
+        description='Convert your pictures into a a Lego mosaic.')
     # parser.add_argument('--server', action='run_server', help='If toggled, displays output to screen')
-    parser.add_argument('--server', nargs='?', const=True, type=bool, default=False, help='Starts a ZMQ server')
-    parser.add_argument('input_filename', metavar='input_filename', type=str, nargs='?', help='The input image to convert')
-    parser.add_argument('output_filename', metavar='output_filename', type=str, const='out.jpg', default='out.jpg', nargs='?', help='The output image to save to')
-    parser.add_argument('--show', nargs='?', const=True, type=bool, default=False, help='If toggled, displays output to screen')
-    parser.add_argument('--size', nargs='?', const=5, type=int, default=5, help='The rendering length of each tile.')
-    parser.add_argument('--length', nargs='?', const=48, type=int, default=48, help='The length of each mosaic.')
-    parser.add_argument('--num_clusters', nargs='?', const=7, type=int, default=7, help='Number of color clusters to quantize')
-    parser.add_argument('--palette_scheme', nargs='?', const='colors2010', type=str, default='colors2010', help='Color palette scheme to use')
+    parser.add_argument('--server', nargs='?', const=True,
+                        type=bool, default=False, help='Starts a ZMQ server')
+    parser.add_argument('input_filename', metavar='input_filename',
+                        type=str, nargs='?', help='The input image to convert')
+    parser.add_argument('output_filename', metavar='output_filename', type=str,
+                        const='out.jpg', default='out.jpg', nargs='?', help='The output image to save to')
+    parser.add_argument('--show', nargs='?', const=True, type=bool,
+                        default=False, help='If toggled, displays output to screen')
+    parser.add_argument('--size', nargs='?', const=5, type=int,
+                        default=5, help='The rendering length of each tile.')
+    parser.add_argument('--length', nargs='?', const=48,
+                        type=int, default=48, help='The length of each mosaic.')
+    parser.add_argument('--num_clusters', nargs='?', const=7, type=int,
+                        default=7, help='Number of color clusters to quantize')
+    parser.add_argument('--palette_scheme', nargs='?', const='colors2010',
+                        type=str, default='colors2010', help='Color palette scheme to use')
     # parser.add_argument('--show', dest='accumulate', action='store_const',
-                    #    const=sum, default=max,
-                    #    help='sum the integers (default: find the max)')
+    #    const=sum, default=max,
+    #    help='sum the integers (default: find the max)')
 
     args = parser.parse_args()
 
     start_server = args.server
     if (start_server):
         run_server(IP, PORT)
-
 
     input_filename = args.input_filename
     output_filename = args.output_filename
@@ -70,16 +79,15 @@ def main():
 
     img = Image(img_length)
     img.load_file(input_filename) \
-    .apply_filter(QuantizeFilter(n)) \
-    .apply_filter(ConstrainPaletteFilter(color_generator, palette_scheme)) \
-    .apply_filter(BuildMapFilter(tile_size)) \
-    .save_file(output_filename)
+        .apply_filter(QuantizeFilter(n)) \
+        .apply_filter(ConstrainPaletteFilter(color_generator, palette_scheme)) \
+        .apply_filter(BuildMapFilter(tile_size)) \
+        .save_file(output_filename)
 
+    img.generate_instructions(tile_size)
 
     if args.show:
         img.show()
-
-
 
 
 if __name__ == '__main__':
--- lego_mosaic/image.py
+++ lego_mosaic/image.py
@@ -4,62 +4,82 @@ from filters import *
 import errno
 import os
 import base64
+import json
 
 
 class Image(object):
     """docstring for Image."""
+
     def __init__(self, length):
         super(Image, self).__init__()
         self.length = length
         self.img = None
 
-
     def load_file(self, filepath):
         self.img = cv2.imread(filepath)
         # Nonexistent input file
         if self.img is None:
-            raise EnvironmentError(errno.ENOENT, os.strerror(errno.ENOENT), filepath)
+            raise EnvironmentError(
+                errno.ENOENT, os.strerror(errno.ENOENT), filepath)
         self.img = cv2.resize(self.img, (self.length, self.length))
         return self
-
 
     def load_str(self, img_str_base64):
         img_str = base64.b64decode(img_str_base64)
         nparr = np.fromstring(img_str, dtype=np.uint8)
-        self.img = cv2.imdecode(nparr, 1) # cv2.IMREAD_COLOR in OpenCV 3.1
+        self.img = cv2.imdecode(nparr, 1)  # cv2.IMREAD_COLOR in OpenCV 3.1
         self.img = cv2.resize(self.img, (self.length, self.length))
         return self
-
 
     def dump_str_base64(self, file_format):
         dump = cv2.imencode('.' + file_format, self.img)[1].tostring()
         return base64.b64encode(dump)
 
-
     def apply_filter(self, filter):
         filter.apply(self)
         return self
-
 
     def show(self):
         cv2.imshow('output', self.img)
         cv2.waitKey(0)
         return self
 
-
     def save_file(self, filepath):
-        cv2.imwrite(filepath,self.img)
+        cv2.imwrite(filepath, self.img)
         return self
+
+    def generate_instructions(self, tile_size):
+        result = {
+            'colorList': []
+        }
+        pic = self.img
+        histogram = {}
+        for y in range(2, len(pic), tile_size):
+            for x in range(2, len(pic[y]), tile_size):
+                (r, g, b) = pic[y - 1][x - 1]
+                if (r, g, b) not in histogram:
+                    print 'a'
+                    histogram[(r, g, b)] = 0
+                histogram[(r, g, b)] = histogram[(r, g, b)] + 1
+
+        for k, v in histogram.iteritems():
+            result['colorList'].append({
+                'color': str(k),
+                'qty': v
+            })
+
+        print json.dumps(result)
+        return json.dumps(result)
 
 
 def main():
     img = Image(48)
     img.load_file('logo17.jpg')
     img \
-    .apply_filter(QuantizeFilter(7)) \
-    .apply_filter(ConstrainPaletteFilter('Lego-colors-palette-2010.gpl.csv')) \
-    .apply_filter(BuildMapFilter(12)) \
-    .show()
+        .apply_filter(QuantizeFilter(7)) \
+        .apply_filter(ConstrainPaletteFilter('Lego-colors-palette-2010.gpl.csv')) \
+        .apply_filter(BuildMapFilter(12)) \
+        .show()
 
 
 if __name__ == '__main__':
